f4b568939b0824109cad46844454d5409be778ab
Gianluca Truda <gianlucatruda@gmail.com>
2017-09-06 00:17:30 +0200

    Added simple read functionality for Firebase DB of segments.

--- dash/js/auct/segment_generator.js
+++ dash/js/auct/segment_generator.js
@@ -16,6 +16,7 @@
 			if(newLabel != "") {
 				s.label = newLabel;
 			}
+			var buttons = document.getElementsByClassName("dropdown-toggle");
 			buttons[s.id].innerText = s.label;
 			console.log("Labelled as '"+s.label+"'")
 		});
@@ -41,62 +42,73 @@
 
 	console.log("Generating segments...");
 
-	for (var i = 0; i < 10; i++) {
-		// Firebase DB query here
-		var s = {
-			id : i,
-			filepath : "segments/sample_fruit_0000"+i+".wav",
-			language : "English",
-			label : "",
-			markup : ""
-		};
+	// Firebase once-off DB query
+	firebase.database().ref('/samples/').once('value').then(function(snapshot) {
+		var dbSegs = snapshot.val();
 
-		// Some Test shit
-		if (s.id == 0) {
-			s.label = "avocado";
-		}
+	  for (var i = 0; i < dbSegs.length; i++) {
+	  	var s = {
+	  		id : dbSegs[i].id,
+	  		filepath : dbSegs[i].filepath,
+	  		language : dbSegs[i].language,
+	  		label : dbSegs[i].label,
+	  		markup : ""
+	  	};
 
-		var type = "primary";
-		var text = "Segment "+s.id.toString();
-		if(s.label != "") {
-			text = s.label;
-			type = "success";
-		}
+	  	// Some Test shit
+	  	if (s.id == 0) {
+	  		s.label = "avocado";
+	  	}
+
+	  	var type = "primary";
+	  	var text = "Segment "+s.id;
+	  	if(s.label != "") {
+	  		text = s.label;
+	  		type = "success";
+	  	}
 
 
-		s.markup = " <div class='btn-group audio-seg'>"
-							+"<a href='#' data-toggle='dropdown' class='btn btn-"+type+" btn-lg dropdown-toggle'>"
-							+text+" <span class='caret'></span></a>"
-							+"<audio id='audio' src='"+s.filepath+"'"
-							+"style='visibility: hidden; width: 0px; height: 0px;'"
-							+"controls preload='auto' autobuffer></audio><ul class='dropdown-menu'"
-							+"role='menu'><li><a href='#' class='button-label'>Label</a></li><li><a href='#'"
-							+"class='button-remove'>Remove</a></li></ul></div>"
+	  	s.markup = " <div class='btn-group audio-seg'>"
+	  						+"<a href='#' data-toggle='dropdown' class='btn btn-"+type+" btn-lg dropdown-toggle'>"
+	  						+text+" <span class='caret'></span></a>"
+	  						+"<audio id='audio' src='"+s.filepath+"'"
+	  						+"style='visibility: hidden; width: 0px; height: 0px;'"
+	  						+"controls preload='auto' autobuffer></audio><ul class='dropdown-menu'"
+	  						+"role='menu'><li><a href='#' class='button-label'>Label</a></li><li><a href='#'"
+	  						+"class='button-remove'>Remove</a></li></ul></div>"
 
-		audioSegs.push(s);
+	  	audioSegs.push(s);
+	  	// console.log("Added "+s.label);
 
-	};
+	  };
 
-	for (var i = 0; i < audioSegs.length; i++) {
-		segPanel.innerHTML += audioSegs[i].markup;
-	}
+	  for (var i = 0; i < audioSegs.length; i++) {
+	  	segPanel.innerHTML += audioSegs[i].markup;
+	  }
 
-	console.log("Linking audio playback...");
+	  console.log("Linking audio playback...");
 
-	var buttons = document.getElementsByClassName("dropdown-toggle");
+	  var buttons = document.getElementsByClassName("dropdown-toggle");
 
-	// TODO Figure out why this won't work in a loop of some kind
-	buttons[0].addEventListener("click", function() {segSelect(audioSegs[0])});
-	buttons[1].addEventListener("click", function() {segSelect(audioSegs[1])});
-	buttons[2].addEventListener("click", function() {segSelect(audioSegs[2])});
-	buttons[3].addEventListener("click", function() {segSelect(audioSegs[3])});
-	buttons[4].addEventListener("click", function() {segSelect(audioSegs[4])});
-	buttons[5].addEventListener("click", function() {segSelect(audioSegs[5])});
-	buttons[6].addEventListener("click", function() {segSelect(audioSegs[6])});
-	buttons[7].addEventListener("click", function() {segSelect(audioSegs[7])});
-	buttons[8].addEventListener("click", function() {segSelect(audioSegs[8])});
-	buttons[9].addEventListener("click", function() {segSelect(audioSegs[9])});
+	  // TODO Figure out why this won't work in a loop of some kind
+	  buttons[0].addEventListener("click", function() {segSelect(audioSegs[0])});
+	  buttons[1].addEventListener("click", function() {segSelect(audioSegs[1])});
+	  buttons[2].addEventListener("click", function() {segSelect(audioSegs[2])});
+	  buttons[3].addEventListener("click", function() {segSelect(audioSegs[3])});
+	  buttons[4].addEventListener("click", function() {segSelect(audioSegs[4])});
+	  buttons[5].addEventListener("click", function() {segSelect(audioSegs[5])});
+	  buttons[6].addEventListener("click", function() {segSelect(audioSegs[6])});
+	  buttons[7].addEventListener("click", function() {segSelect(audioSegs[7])});
+	  buttons[8].addEventListener("click", function() {segSelect(audioSegs[8])});
+	  buttons[9].addEventListener("click", function() {segSelect(audioSegs[9])});
 
+	  console.log("Ready.");
+
+
+	}).catch(function(db_error) {
+		console.log("Error loading from DB");
+		location.reload();
+	});
 
 
 }());
