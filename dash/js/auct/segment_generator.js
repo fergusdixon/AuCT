cdca907821c1329fe160f047161e3643e3dab965
Gianluca Truda <gianlucatruda@gmail.com>
2017-09-05 19:10:17 +0200

    Refactored segment_generator and changed audio playback method. Works, but has a bug.

--- dash/js/auct/segment_generator.js
+++ dash/js/auct/segment_generator.js
@@ -3,8 +3,11 @@
 	var segPanel = document.getElementsByClassName("segment-holder")[0];
 	var audioSegs = [];
 
-	var play = function (s) {
-		document.getElementById("audio").play();
+	var playSeg = function (s) {
+		console.log("Playing "+s.id);
+		var audio = document.createElement('audio');
+		audio.src = "segments/sample_fruit_00003.wav",
+		audio.play();
 	}
 
 	console.log("Generating segments...");
@@ -14,16 +17,17 @@
 		var s = {
 			id : i,
 			filepath : "segments/sample_fruit_0000"+i+".wav",
+			markup : " <div class='btn-group audio-seg'>"
+								+"<a href='#' data-toggle='dropdown' class='btn btn-primary btn-lg dropdown-toggle'>"
+								+"SEGMENT 1 <span class='caret'></span></a>"
+								+"<audio id='audio' src='"+"segments/sample_fruit_0000"+i+".wav"+"'"
+								+"style='visibility: hidden; width: 0px; height: 0px;'"
+								+"controls preload='auto' autobuffer></audio><ul class='dropdown-menu'"
+								+"role='menu'><li><a href='#'>Label</a></li><li><a href='#'>Remove"
+								+"</a></li></ul></div>"
 		};
 
-		segPanel.innerHTML += " <div class='btn-group audio-seg'>"
-													+"<a href='#' data-toggle='dropdown' class='btn btn-primary btn-lg dropdown-toggle'>"
-													+"SEGMENT 1 <span class='caret'></span></a>"
-													+"<audio id='audio' src='"+s.filepath+"'"
-													+"style='visibility: hidden; width: 0px; height: 0px;'"
-													+"controls preload='auto' autobuffer></audio><ul class='dropdown-menu'"
-													+"role='menu'><li><a href='#'>Label</a></li><li><a href='#'>Remove"
-													+"</a></li></ul></div>"
+		segPanel.innerHTML += s.markup;
 
 		audioSegs.push(s);
 
@@ -32,10 +36,16 @@
 	console.log("Linking audio playback...");
 
 	var audio_segments = document.getElementsByClassName("audio-seg");
-	var myseg = audio_segments[0];
 
-	myseg.onclick = function(myseg) {
-		play(myseg);
-	};
+	for (var i = 0; i < 10 ; i++) {
+		var myseg = audio_segments[i];
+		myseg.onclick = function(myseg) {
+			// TODO Fix this next line
+			console.log(audioSegs[i].id);
+			// playSeg(audioSegs[i]);
+		};
+	}
+
+
 
 }());
