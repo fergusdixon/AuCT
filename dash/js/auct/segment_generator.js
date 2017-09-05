686064686357e5816adce523409822c4e1b74892
Gianluca Truda <gianlucatruda@gmail.com>
2017-09-05 22:11:21 +0200

    10 sample segments playback working. Deprecated audio_playback script.

--- dash/js/auct/segment_generator.js
+++ dash/js/auct/segment_generator.js
@@ -1,25 +1,28 @@
 (function (){
 
+	// Get a reference to the Firebase database service
+	var database = firebase.database();
+
 	var segPanel = document.getElementsByClassName("segment-holder")[0];
 	var audioSegs = [];
 
 	var playSeg = function (s) {
 		console.log("Playing "+s.id);
 		var audio = document.createElement('audio');
-		audio.src = "segments/sample_fruit_00003.wav",
+		audio.src = s.filepath;
 		audio.play();
 	}
 
 	console.log("Generating segments...");
 
 	for (var i = 0; i < 10; i++) {
-
+		// Firebase DB query here
 		var s = {
 			id : i,
 			filepath : "segments/sample_fruit_0000"+i+".wav",
 			markup : " <div class='btn-group audio-seg'>"
 								+"<a href='#' data-toggle='dropdown' class='btn btn-primary btn-lg dropdown-toggle'>"
-								+"SEGMENT 1 <span class='caret'></span></a>"
+								+"SEGMENT "+i+" <span class='caret'></span></a>"
 								+"<audio id='audio' src='"+"segments/sample_fruit_0000"+i+".wav"+"'"
 								+"style='visibility: hidden; width: 0px; height: 0px;'"
 								+"controls preload='auto' autobuffer></audio><ul class='dropdown-menu'"
@@ -35,16 +38,19 @@
 
 	console.log("Linking audio playback...");
 
-	var audio_segments = document.getElementsByClassName("audio-seg");
+	var buttons = document.getElementsByClassName("dropdown-toggle");
 
-	for (var i = 0; i < 10 ; i++) {
-		var myseg = audio_segments[i];
-		myseg.onclick = function(myseg) {
-			// TODO Fix this next line
-			console.log(audioSegs[i].id);
-			// playSeg(audioSegs[i]);
-		};
-	}
+	// TODO Figure out why this won't work in a loop of some kind
+	buttons[0].addEventListener("click", function() {playSeg(audioSegs[0])});
+	buttons[1].addEventListener("click", function() {playSeg(audioSegs[1])});
+	buttons[2].addEventListener("click", function() {playSeg(audioSegs[2])});
+	buttons[3].addEventListener("click", function() {playSeg(audioSegs[3])});
+	buttons[4].addEventListener("click", function() {playSeg(audioSegs[4])});
+	buttons[5].addEventListener("click", function() {playSeg(audioSegs[5])});
+	buttons[6].addEventListener("click", function() {playSeg(audioSegs[6])});
+	buttons[7].addEventListener("click", function() {playSeg(audioSegs[7])});
+	buttons[8].addEventListener("click", function() {playSeg(audioSegs[8])});
+	buttons[9].addEventListener("click", function() {playSeg(audioSegs[9])});
 
 
 
