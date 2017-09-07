32a11289be721e30aea385df20f11436d20d3f75
Fergus <strfer001@myuct.ac.za>
2017-09-07 17:48:04 +0200

    Considering getting rid of the wrapper to use ffmpeg directly

--- AuctServer/Server/Segmentor.py
+++ AuctServer/Server/Segmentor.py
@@ -6,13 +6,11 @@ import ffmpeg
 import sys
 
 def segment(inputFile, outputDirectory):
-    sys.stdout = open('test.txt', 'w')
     stream = ffmpeg.input(inputFile)
-    stream = stream.
     stream = ffmpeg.filter_(stream, 'silencedetect', noise='-30dB', d='0.5')
     stream = ffmpeg.output(stream, 'test.wav')
-    ffmpeg.run(stream)
-    print("Hey")
+    x = ffmpeg.run(stream)
+    print(x)
 
 
 segment("sample_fruit_quiet.wav", "test.wav")
