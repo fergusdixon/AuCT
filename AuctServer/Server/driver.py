33f52b83079d5eebb36a2d3e844be9ec84cc88a9
Fergus <strfer001@myuct.ac.za>
2017-09-11 12:59:36 +0200

    Made the demo command-line friendly, in the build we submitted it would only run through pycharm

--- AuctServer/Server/driver.py
+++ AuctServer/Server/driver.py
@@ -1,7 +1,7 @@
 # @Author   Fergus Strangways-Dixon
 # @Date     7/09/2017
 # This is the main server for handling AuCT request
-from Server import Segmentor
+import segmentor
 
 
 def main():
@@ -9,7 +9,7 @@ def main():
                 "Please ensure your file is in the 'AuctServer/input' directory\n"
                 "Enter the file name, including extension: ")
     print("* Processing " + inp + " *")
-    Segmentor.segment(inp)
+    segmentor.segment(inp)
 
 
 if __name__ == '__main__':
