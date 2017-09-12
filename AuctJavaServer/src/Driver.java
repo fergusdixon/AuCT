b2bcd4e398d0d0033f2f4d6c3bb6a302cfa16e63
Fergus <fergusdixon101@gmail.com>
2017-09-12 23:19:38 +0200

    Added gradle for firebase integration

--- AuctJavaServer/src/Driver.java
+++
@@ -1,8 +0,0 @@
-public class Driver {
-
-    public static void main(String[] args) {
-        Segmentor seg = new Segmentor("/home/fergus/AuCT/AuctJavaServer/src/");
-
-        System.out.println(seg.segment("sample_fruit_quiet.wav"));
-    }
-}
