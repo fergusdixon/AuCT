64c5f3eb339d7a1a6e2aa7bc3d637490dc5e6798
Fergus <fergusdixon101@gmail.com>
2017-09-28 15:07:33 +0200

    Added a buunch of stuff

--- AuctJavaServer/src/main/java/server/Driver.java
+++ AuctJavaServer/src/main/java/server/Driver.java
@@ -11,10 +11,67 @@ import java.io.*;
 
 public class Driver {
 
+    private static Bucket bucket = null;
+    private static String fileName;
 
     public static void main(String[] args) {
+        System.out.println("Retrieving cloud storage...");
+        login();
+        System.out.println("Success");
+
+        fileName = "sample_sports_television";
+        System.out.println("Downloading: " + fileName + "...");
+        //getAudio(fileName);
+
+        System.out.println("Success, analysing and segmenting the audio file...");
+        if(split()){
+            System.out.println("Splitting of \"" + fileName + "\" successful.");
+        }
+        else {
+            System.out.println("Splitting failed");
+        }
+    }
+
+    private static boolean split(){
+        Segmentor seg = new Segmentor("/home/fergus/AuCT/AuctJavaServer/src/main/java/server/");
+
+        return seg.segment(fileName+".wav");
+    }
+
+    private static void getAudio(String name){
+        //getting file
+        Blob blob = bucket.get("Input/" + name);
+        byte[] array = blob.getContent();
+
+        String uncutName = blob.getName();
+
+        fileName = blob.getName().substring(
+                uncutName.indexOf('/')+1,
+                uncutName.indexOf('.')
+        );
+
+        File f = null;
+        try {
+            String path = "/home/fergus/AuCT/AuctJavaServer/src/input/" + fileName + ".wav";
+            f = new File(path);
+
+            f.getParentFile().mkdirs();
+            f.createNewFile();
+            FileOutputStream out = new FileOutputStream(f);
+            out.write( array );
+            out.close();
+            f.deleteOnExit();
+            System.out.println("File written");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void login(){
         //getting the storage folder from firebase
-        Bucket bucket = null;
+
         try{
             FileInputStream serviceAccount = new FileInputStream("/home/fergus/AuCT/AuctJavaServer/auct-capstone-firebase-adminsdk-57nym-694062f77b.json");
 
@@ -31,38 +88,5 @@ public class Driver {
         catch(Exception e){
             System.out.println("Firebase error:\n" + e);
         }
-
-
-        //getting file
-        Blob blob = bucket.get("Input/sample_fruit_quiet.wav");
-        byte[] array = blob.getContent();
-
-        String uncutName = blob.getName();
-
-        String fileName = blob.getName().substring(
-                uncutName.indexOf('/')+1,
-                uncutName.indexOf('.')
-        );
-
-        File f = null;
-        try {
-            String path = "/home/fergus/AuCT/AuctJavaServer/src/input/" + fileName + ".wav";
-            f = new File(path);
-
-            f.getParentFile().mkdirs();
-            f.createNewFile();
-            FileOutputStream out = new FileOutputStream(f);
-            out.write( array );
-            out.close();
-            System.out.println("File written");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-        System.out.println("Splitting");
-        Segmentor seg = new Segmentor("/home/fergus/AuCT/AuctJavaServer/src/main/java/server/");
-
-        System.out.println(seg.segment(fileName+".wav"));
     }
 }
