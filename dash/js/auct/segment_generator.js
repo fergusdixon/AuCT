1c1f3bb6cd2d49b406d0df3712f683166c53a856
Gianluca Truda <gianlucatruda@gmail.com>
2017-09-29 02:00:51 +0200

    Responsive layout, dynamic generation, cloud audio embedding all works. Still having issues with event listeners for buttons.

--- dash/js/auct/segment_generator.js
+++ dash/js/auct/segment_generator.js
@@ -3,6 +3,9 @@ function loadSeg(sid, wlref) {
 
 	// Get a reference to the Firebase database service
 	var database = firebase.database();
+	// Get a reference to the storage service, which is used to create references in your storage bucket
+	var storage = firebase.storage();
+	var storageRef = storage.ref('/');
 
 	var segPanel = document.getElementsByClassName("segment-holder")[sid];
 	var audioSegs = [];
@@ -20,12 +23,14 @@ function loadSeg(sid, wlref) {
 		firebase.database().ref('/segments/').once('value').then(function(snapshot) {
 			var dbSegs = snapshot.val();
 
-			for (var i = 0; i < dbSegs.length; i++) {
+			for (var i = 0; i < 3; i++) { //for (var i = 0; i < dbSegs.length; i++) {
 
 				if(dbSegs[i].session == sid) {
+
 					var s = {
 						id : dbSegs[i].id,
 						filepath : dbSegs[i].filepath,
+						url : "",
 						label : dbSegs[i].label,
 						scrapped : dbSegs[i].scrapped,
 						session : dbSegs[i].session,
@@ -47,24 +52,48 @@ function loadSeg(sid, wlref) {
 					}
 
 					s.markup = "<button class='btn btn-"+type+" btn-rounded btn-segment'>"+s.label+"</button>"
-										+"<audio id='audio' src='"+"'" //TODO add source as cloud folder on Firebase
-										+"style='visibility: hidden; width: 0px; height: 0px;'"
-										+"controls preload='auto' autobuffer></audio>"
 										+suggestions+"<br>";
 
 					audioSegs.push(s);
+
 				}
 
 			};
 
 			segPanel.innerHTML = "";
+
 			for (var i = 0; i < audioSegs.length; i++) {
+
 				segPanel.innerHTML += audioSegs[i].markup;
 			}
 
-			// console.log("Linking audio playback...");
+			console.log("Linking audio playback...");
+
+			for (var i = audioSegs.length - 1; i >= 0; i--) {
+				var s = audioSegs[i];
+				// Create a reference to the file we want to download
+				var audioRef = storageRef.child(s.filepath);
+				// Get the download URL
+				audioRef.getDownloadURL().then(function(url) {
+					s.url = url;
+					// console.log("loaded url: "+url);
+
+				}).catch(function(error) {
+					console.log("couldn't load url");
+				});
+			}
 
 			// TODO make audio play on mouseover
+			var roundButtons = document.getElementsByClassName('btn-segment');
+
+			for (var i = roundButtons.length - 1; i >= 0; i--) {
+				roundButtons[i].addEventListener("mouseover", function() {
+					var audio = document.createElement('audio');
+					audio.src = audioSegs[0].url;
+					audio.play();
+				});
+
+			}
 
 			console.log("Loaded session "+sid);
 
