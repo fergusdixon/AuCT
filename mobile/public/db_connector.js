f54bf45ba700ee2e65a6319655a3dfdded51616c
BrigitteSprung <brigittesprung@gmail.com>
2017-10-04 00:09:25 +0200

    WordList Population
    - shows the words from the list chosen in the previous page
    - parameters passed through hash
    - list is scrollable by touch

--- mobile/public/db_connector.js
+++ mobile/public/db_connector.js
@@ -1,15 +1,33 @@
 console.log("Opening word list page");
 
+// Get the file path of the current window
+var url=location.href;
+var urlFilename = url.substring(url.lastIndexOf('/')+1);
+console.log(urlFilename);
 
-var wordlists = [];
+// Initialise needed variablses
+
+var wordlistNo = 0;
+var chosenList = null;
+var pagehash = null;
+// Get reference for firebase database
 var database = firebase.database();
 console.log("connected to database")
 
-var chosenList = null;
+// Run functions according to needed
+if (urlFilename == "wordListSelection.html"){
+	getWordLists();
+	addButtons();
+} else {
+	pagehash = window.location.hash;
+	console.log("hash: " + pagehash);
+	populateWordListOnRecordAudio();
+}
 
+
+function getWordLists() {
 firebase.database().ref('/wordlists/').once('value').then(function(snapshot) {
 	wordlists = snapshot.val();
-
 	var divId = document.getElementById("wordlistRow");
 
 	snapshot.forEach(function(childSnapshot){
@@ -19,24 +37,73 @@ firebase.database().ref('/wordlists/').once('value').then(function(snapshot) {
 		console.log("data: " + childData);
 		var childName = childData.name;
 		console.log("name: " + childName)
-		var listButton = document.createElement('button');
-		listButton.className = 'btn btn-default';
-		listButton.innerText = childName;
-		listButton.href = "recordAudio.html";
-		listButton.addEventListener("click", function(){
-			chosenList = childData;
-			console.log("Button clicked. List chosen is: " + childData.name);
-			window.location.href = ("recordAudio.html");
-		});
-
+		wordlistNo++;
+		console.log("starting button creation...")
+	
+	var listButton = createButtonToSelectionPage(childData);
 		divId.appendChild(listButton);
 		console.log("wordlist button made")
 
+
 	})
+	console.log(wordlistNo);
 }).catch(function(db_error) {
 		console.log("Error loading wordlists: " + db_error);
 
 	});
+
+}
+
+function createButtonToSelectionPage(childData){
+	console.log("adding a button")
+	var listButton = document.createElement('button');
+	listButton.className = 'btn btn-default';
+	listButton.innerText = childData.name;
+	listButton.href = "recordAudio.html#" + childData.listnum;
+	console.log("button href: " + listButton.href)
+	listButton.addEventListener("click", function(){
+		chosenList = childData;
+		console.log("Button clicked. List chosen is: " + childData.name);
+		window.location.href = ("recordAudio.html#" + childData.listnum);
+		populateWordListOnRecordAudio();
+	});
+	return listButton;
+}
+
+function populateWordListOnRecordAudio(){
+
+	firebase.database().ref('/wordlists/').once('value').then(function(snapshot) {
+		console.log("accessed database");
+	var listGroupID = document.getElementById("wordlistgroup");
+	var listHeadingID = document.getElementById("listheading");
+	if (pagehash == null) {
+		var listNo = parseInt(0);
+	} else {
+		var listNo = parseInt(pagehash.substring(1)) - 1;
+	}
+	
+	snapshot.forEach(function(childSnapshot){
+		console.log("key: " + childSnapshot.key + ", listNo: " + listNo);
+	if (childSnapshot.key == listNo){
+		console.log(childSnapshot.val().name);
+		listHeadingID.innerText = childSnapshot.val().name;
+		var list = childSnapshot.val().words;
+		for (var i = 0; i < list.length; i++) {
+			var li = document.createElement("li");
+			li.className = "list-group-item";
+			li.innerText = list[i];
+			listGroupID.appendChild(li);
+			console.log("added item: " + list[i]);
+	}
+	}
+
+})
+	}).catch(function(db_error) {
+		console.log("Error loading wordlists: " + db_error);
+
+	});
+
+}
 
 
 
