7dd75ef107ea4f951ba136e9e43775326b9a3513
BrigitteSprung <brigittesprung@gmail.com>
2017-10-04 23:29:45 +0200

    Added comments, fixed filename and date formats, fixed some UI problems

--- mobile/public/audio_recorder_new.js
+++ mobile/public/audio_recorder_new.js
@@ -1,243 +1,255 @@
-// Expose globally your audio_context, the recorder instance and audio_stream
-        var audio_context;
-        var recorder;
-        var audio_stream;
-        var canvas;
-        var canvasCtx;
-        var audioCtx;
-        var mainSection;
-        var _language;
-        var _name;
-        var _key;
+// Expose globally these variables
+var audio_context;
+var recorder;
+var audio_stream;
+var canvas;
+var canvasCtx;
+var audioCtx;
+var mainSection;
+var _language;
+var _name;
+var _key;
 
-        /**
-         * Patch the APIs for every browser that supports them and check
-         * if getUserMedia is supported on the browser. 
-         * 
-         */
-        function Initialize() {
-            try {
-                // Monkeypatch for AudioContext, getUserMedia and URL
-                window.AudioContext = window.AudioContext || window.webkitAudioContext;
-                navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
-                window.URL = window.URL || window.webkitURL;
-
-                // Store the instance of AudioContext globally
-                audio_context = new AudioContext;
-                console.log('Audio context is ready !');
-                console.log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
-
-            } catch (e) {
-                alert('No web audio support in this browser!');
-            }
-        }
+/**
+ * Check if getUserMedia is supported on the browser
+ * Create instances of AudioContext
+ */
+function Initialize() {
+    try {
+        // Monkeypatch for AudioContext, getUserMedia and URL
+        window.AudioContext = window.AudioContext || window.webkitAudioContext;
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+        window.URL = window.URL || window.webkitURL;
+        // Store the instance of AudioContext globally
+        audio_context = new AudioContext;
+        console.log('Audio context is ready !');
+        console.log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
+} catch (e) {
+        alert('No web audio support in this browser!');
+    }
+}
  
-        /**
-         * Starts the recording process by requesting the access to the microphone.
-         * Then, if granted proceed to initialize the library and store the stream.
-         *
-         * It only stops when the method stopRecording is triggered.
-         */
-        function startRecording() {
-            // Access the Microphone using the navigator.getUserMedia method to obtain a stream
-            navigator.getUserMedia({ audio: true }, function (stream) {
-                // Expose the stream to be accessible globally
-                audio_stream = stream;
-                // Create the MediaStreamSource for the Recorder library
-                var input = audio_context.createMediaStreamSource(stream);
-                console.log('Media stream succesfully created');
+/**
+ * Starts the recording process by requesting the access to the microphone.
+ * Then, if granted proceed to initialize the Recorder library and store the stream.
+ *
+ * It only stops when the method stopRecording is triggered.
+ */
+function startRecording() {
+    // Access the Microphone using the navigator.getUserMedia method to obtain a stream
+    navigator.getUserMedia({ audio: true }, function (stream) {
+        // Expose the stream to be accessible globally
+        audio_stream = stream;
+        // Create the MediaStreamSource for the Recorder library
+        var input = audio_context.createMediaStreamSource(stream);
+        console.log('Media stream succesfully created');
 
-                // Initialize the Recorder Library
-                recorder = new Recorder(input);
-                console.log('Recorder initialised');
+        // Initialize the Recorder Library
+        recorder = new Recorder(input);
+        console.log('Recorder initialised');
 
-                // Start recording !
-                recorder && recorder.record();
-                console.log('Recording...');
+        // Start recording
+        recorder && recorder.record();
+        console.log('Recording...');
 
-                // Disable Record button and enable stop button !
-                
-                document.getElementById("recordbtn").disabled = true;
-                document.getElementById("stopbtn").disabled = false;
-            }, function (e) {
-                console.error('No live audio input: ' + e);
-            });
-        }
+        // Disable Record button and enable stop button        
+        document.getElementById("recordbtn").disabled = true;
+        document.getElementById("stopbtn").disabled = false;
 
-        /**
-         * Stops the recording process. The method expects a callback as first
-         * argument (function) executed once the AudioBlob is generated and it
-         * receives the same Blob as first argument. The second argument is
-         * optional and specifies the format to export the blob either wav or mp3
-         */
-        function stopRecording(callback, AudioFormat) {
-            // Stop the recorder instance
-            recorder && recorder.stop();
-            console.log('Stopped recording.');
+    }, function (e) {
+        console.error('No live audio input: ' + e);
+    });
+}
 
-            // Stop the getUserMedia Audio Stream !
-            audio_stream.getAudioTracks()[0].stop();
+/**
+ * Stops the recording process. The method expects a callback as first
+ * argument (function) executed once the AudioBlob is generated and it
+ * receives the same Blob as first argument. The second argument is
+ * optional and specifies the format to export the blob either wav or mp3
+ */
+function stopRecording(callback, AudioFormat) {
+    // Stop the recorder instance
+    recorder && recorder.stop();
+    console.log('Stopped recording.');
 
-            // Disable Stop button and enable Record button !
-            document.getElementById("recordbtn").disabled = false;
-            document.getElementById("stopbtn").disabled = true;
+    // Stop the getUserMedia Audio Stream
+    audio_stream.getAudioTracks()[0].stop();
 
-            // Use the Recorder Library to export the recorder Audio as a .wav file
-            // The callback providen in the stop recording method receives the blob
-            if(typeof(callback) == "function"){
+    // Disable Stop button and enable Record button
+    document.getElementById("recordbtn").disabled = false;
+    document.getElementById("stopbtn").disabled = true;
 
-                /**
-                 * Export the AudioBLOB using the exportWAV method.
-                 * Note that this method exports too with mp3 if
-                 * you provide the second argument of the function
-                 */
-                recorder && recorder.exportWAV(function (blob) {
-                    callback(blob);
+    // Use the Recorder Library to export the recorder Audio as a .wav file
+    // The callback providen in the stop recording method receives the blob
+    if(typeof(callback) == "function"){
 
-                    // create WAV download link using audio data blob
-                    // createDownloadLink();
+    /**
+     * Export the AudioBLOB using the exportWAV method from Recorder library.
+     */
+    recorder && recorder.exportWAV(function (blob) {
+        callback(blob);
 
-                    // Clear the Recorder to start again !
-                    recorder.clear();
-                }, (AudioFormat || "audio/wav"));
+        // create WAV download link using audio data blob
+        // createDownloadLink();
+
+        // Clear the Recorder to start again !
+        recorder.clear();
+        }, (AudioFormat || "audio/wav"));
+    }
+}
+
+/** 
+* This is what starts once the recordAudio.html file is opened
+* Initializes everything once the window loads
+*/
+window.onload = function(){
+    // Prepare and check if requirements are filled
+    Initialize();
+
+    // Set up canvas for waveform
+    canvas = document.querySelector('.visualizer');
+    mainSection = document.querySelector('.main-controls');
+
+    // visualiser setup - create web audio api context and canvas
+    var canvasCtx = canvas.getContext("2d");
+    console.log("visualiser set up")
+
+    // Handle on start recording button
+    document.getElementById("recordbtn").addEventListener("click", function(){
+        startRecording();
+        //console.log("Audio_stream: " + typeof audio_stream);
+        //visualize(audio_stream);
+    }, false);
+
+    // Handle on stop recording button
+    document.getElementById("stopbtn").addEventListener("click", function(){
+        // Set audio format as wav format
+        var _AudioFormat = "audio/wav";
+
+        stopRecording(function(AudioBLOB){
+
+            // Create an Audio item and related components to the list to allow for playback of every stored Audio
+            var url = URL.createObjectURL(AudioBLOB);
+            var container = document.createElement('div');
+            var audio = document.createElement('audio');
+            var href = document.createElement('a');
+            var hrefrow = document.createElement('div');
+            var audiorow = document.createElement('div');
+            var buttonrow = document.createElement('div');
+
+            // Create buttons to submit or delete
+            var submitbtn = document.createElement('button');
+            var deletebtn = document.createElement('button');
+            submitbtn.innerText = "Submit";
+            deletebtn.innerText = "Delete";
+            deletebtn.className = 'btn btn-default';
+            submitbtn.className = 'btn btn-default';
+
+            // Set layout and look for each item
+            container.className = "container-fluid"
+            hrefrow.className = 'row';
+            audiorow.className = 'row';
+            buttonrow.className = 'row';
+
+
+            // Set controls for audio player
+            audio.controls = true;
+            audio.src = url;
+            href.href = url;
+
+            // Add components to UI
+            href.download = filename;
+            href.innerHTML = href.download;
+            audiorow.appendChild(audio);
+            hrefrow.appendChild(href);
+            buttonrow.appendChild(submitbtn);
+            buttonrow.appendChild(deletebtn);
+            container.appendChild(hrefrow);
+            container.appendChild(audiorow);
+            container.appendChild(buttonrow);
+            var recordingslist = document.getElementById("recordingslist");
+            recordingslist.appendChild(container);
+
+            // Get information for naming of file
+            // Format: auct_list00_YYYYmmDDhhMMss.wav
+            var now = new Date(); 
+            var datestring = getDateFormatForLabel(now);
+            var filedate = getDateFormatForFile(now);
+            var filename = getNameFormat(filedate);
+            
+            // Submit that specific audio file to the firebase storage
+            submitbtn.onclick = function(){
+                upload(AudioBLOB, filedate, filename);
+                createSession(datestring, filename);
+
             }
-        }
 
+            // Delete that specific audio file and surrounding components
+            deletebtn.onclick = function(e){
+                evtTgt = e.target;
+                evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+            }
+        }, _AudioFormat);
+    }, false);
+};
 
-        // Initialize everything once the window loads
-        window.onload = function(){
-            // Prepare and check if requirements are filled
-            Initialize();
-
-            // Set up canvas for waveform
-            canvas = document.querySelector('.visualizer');
-            mainSection = document.querySelector('.main-controls');
-
-            // visualiser setup - create web audio api context and canvas
-
-            var canvasCtx = canvas.getContext("2d");
-            console.log("visualiser set up")
-
-            // Handle on start recording button
-            document.getElementById("recordbtn").addEventListener("click", function(){
-                startRecording();
-                console.log("Audio_stream: " + typeof audio_stream);
-                //visualize(audio_stream);
-            }, false);
-
-            // Handle on stop recording button
-            document.getElementById("stopbtn").addEventListener("click", function(){
-                // Use wav format
-                var _AudioFormat = "audio/wav";
-                // You can use mp3 to using the correct mimetype
-                //var AudioFormat = "audio/mpeg";
-
-                stopRecording(function(AudioBLOB){
-                    // Note:
-                    // Use the AudioBLOB for whatever you need, to download
-                    // directly in the browser, to upload to the server, you name it !
-
-                    // In this case we are going to add an Audio item to the list so you
-                    // can play every stored Audio
-                    var url = URL.createObjectURL(AudioBLOB);
-                    var li = document.createElement('li');
-                    var au = document.createElement('audio');
-                    var hf = document.createElement('a');
-
-                    // Create buttons to submit or delete
-                    var submitbtn = document.createElement('button');
-                    var deletebtn = document.createElement('button');
-                    submitbtn.innerText = "Submit";
-                    deletebtn.innerText = "Delete";
-                    deletebtn.className = 'btn btn-default';
-                    submitbtn.className = 'btn btn-default';
-
-                    au.controls = true;
-                    au.src = url;
-                    hf.href = url;
-                    var now = new Date();
-                    var datestring = now.toISOString();
-                    var filedate = getDateFormat(now);
-                    var filename = getNameFormat(filedate);
-                    hf.download = filename;
-                    hf.innerHTML = hf.download;
-                    li.appendChild(au);
-                    li.appendChild(hf);
-                    li.appendChild(submitbtn);
-                    li.appendChild(deletebtn);
-                    var recordingslist = document.getElementById("recordingslist");
-                    recordingslist.appendChild(li);
-
-                    // Submit that specific file to the firebase storage
-                    submitbtn.onclick = function(){
-                       upload(AudioBLOB, filedate, filename);
-                       createSession(datestring, filename);
-
-                    }
-
-                    // Delete that specific audio file and surrounding buttons
-                    deletebtn.onclick = function(e){
-                        evtTgt = e.target;
-                        evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
-                    }
-                }, _AudioFormat);
-            }, false);
-        };
-
-        // Creates the waveform
+/**
+* Uses MediaStream to analyse audio
+* Produces a waveform of the audio
+*/
 function visualize(stream) {
     console.log(typeof stream);
-  var source = audio_context.createMediaStreamSource(stream);
+    var source = audio_context.createMediaStreamSource(stream);
 
-  var analyser = audio_context.createAnalyser();
-  analyser.fftSize = 2048;
-  var bufferLength = analyser.frequencyBinCount;
-  var dataArray = new Uint8Array(bufferLength);
+    var analyser = audio_context.createAnalyser();
+    analyser.fftSize = 2048;
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
 
-  source.connect(analyser);
-  //analyser.connect(audioCtx.destination);
+    source.connect(analyser);
+    //analyser.connect(audioCtx.destination);
 
-  draw()
+    draw()
 
-  // Draws the waveform
-  function draw() {
-    WIDTH = canvas.width
-    HEIGHT = canvas.height;
+    // Draw the wave form onto the screen
+    function draw() {
+        WIDTH = canvas.width
+        HEIGHT = canvas.height;
 
-    requestAnimationFrame(draw);
+        requestAnimationFrame(draw);
 
-    analyser.getByteTimeDomainData(dataArray);
+        analyser.getByteTimeDomainData(dataArray);
 
-    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
-    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+        canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+        canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
 
-    canvasCtx.lineWidth = 2;
-    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+        canvasCtx.lineWidth = 2;
+        canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
 
-    canvasCtx.beginPath();
+        canvasCtx.beginPath();
 
-    var sliceWidth = WIDTH * 1.0 / bufferLength;
-    var x = 0;
+        var sliceWidth = WIDTH * 1.0 / bufferLength;
+        var x = 0;
 
 
-    for(var i = 0; i < bufferLength; i++) {
+        for(var i = 0; i < bufferLength; i++) {
  
-      var v = dataArray[i] / 128.0;
-      var y = v * HEIGHT/2;
+        var v = dataArray[i] / 128.0;
+        var y = v * HEIGHT/2;
 
-      if(i === 0) {
-        canvasCtx.moveTo(x, y);
-      } else {
-        canvasCtx.lineTo(x, y);
-      }
+        if(i === 0) {
+            canvasCtx.moveTo(x, y);
+        } else {
+            canvasCtx.lineTo(x, y);
+        }
 
-      x += sliceWidth;
+        x += sliceWidth;
+        }
+
+        canvasCtx.lineTo(canvas.width, canvas.height/2);
+        canvasCtx.stroke();
+
     }
-
-    canvasCtx.lineTo(canvas.width, canvas.height/2);
-    canvasCtx.stroke();
-
-  }
 }
 
 // // Changes size, specifically waveform box
--- mobile/public/db_connector.js
+++ mobile/public/db_connector.js
@@ -1,13 +1,16 @@
+// Expose variables globally
+
+var wordlistNo = 0;
+var chosenList = null;
+var pagehash = null;
+var listNo;
+
 // Get the file path of the current window
 var url=location.href;
 var urlFilename = url.substring(url.lastIndexOf('/')+1);
 // DEBUG: console.log(urlFilename);
 
-// Initialise needed variablses
 
-var wordlistNo = 0;
-var chosenList = null;
-var pagehash = null;
 // Get reference for firebase database
 var database = firebase.database();
 console.log("Connected to database");
@@ -15,9 +18,8 @@ console.log("Connected to database");
 var storage = firebase.storage();
 console.log("Connected to storage bucket");
 
-var listNo;
 
-// Run functions according to needed
+// Use filepath name to decide which functions to start with
 if (urlFilename == "wordListSelection.html"){
 	getWordLists();
 } else {
@@ -26,39 +28,51 @@ if (urlFilename == "wordListSelection.html"){
 	populateWordListOnRecordAudio();
 }
 
-
+/**
+* Returns all wordlists from the database
+* Gets data for each individual node
+* Creates a button for each list
+* User selects from one of these
+*/
 function getWordLists() {
-firebase.database().ref('/wordlists/').once('value').then(function(snapshot) {
-	wordlists = snapshot.val();
-	var divId = document.getElementById("wordlistRow");
+	// Goes into 'wordlists' node of database
+	firebase.database().ref('/wordlists/').once('value').then(function(snapshot) {
+		wordlists = snapshot.val();
+		var divId = document.getElementById("wordlistRow");
 
-	snapshot.forEach(function(childSnapshot){
-		var childKey = childSnapshot.key;
-		var childData = childSnapshot.val();
-		var childName = childData.name;
-		var childLanguage = childData.language;
-		wordlistNo++;
-	
-	var listButton = createButtonToSelectionPage(childData);
-		divId.appendChild(listButton);
-		console.log("Wordlist button made")
+		// Get data for each child
+		snapshot.forEach(function(childSnapshot){
+			var childKey = childSnapshot.key;
+			var childData = childSnapshot.val();
+			var childName = childData.name;
+			var childLanguage = childData.language;
+			wordlistNo++;
+		
+			// Create a button for the child node
+			var listButton = createButtonToSelectionPage(childData);
+			divId.appendChild(listButton);
+			console.log("Wordlist button made")
 
+		})
+		console.log(wordlistNo);
+	}).catch(function(db_error) {
+			console.log("Error loading wordlists: " + db_error);
 
-	})
-	console.log(wordlistNo);
-}).catch(function(db_error) {
-		console.log("Error loading wordlists: " + db_error);
-
-	});
+		});
 
 }
 
+/**
+* Create button for child node
+*/
 function createButtonToSelectionPage(childData){
 	var listButton = document.createElement('button');
 	listButton.className = 'btn btn-default';
 	listButton.innerText = childData.name;
-	// listButton.href = "recordAudio.html#" + childData.listnum + "." + childData.language + "." + childData.name;
 	// DEBUG: console.log("button href: " + listButton.href)
+
+	// Sets it so that user is transferred to recordAudio.html page
+	// Passes data about the wordlist chosen via the url hash
 	listButton.addEventListener("click", function(){
 		chosenList = childData;
 		console.log("Button clicked. List chosen is: " + childData.name);
@@ -68,35 +82,43 @@ function createButtonToSelectionPage(childData){
 	return listButton;
 }
 
+/**
+* Gets the word list chosen by the user in the previous page
+* Access all words from the wordlist
+* Add a list element to the UI
+*/
 function populateWordListOnRecordAudio(){
-
+	// References to 'wordlists'
 	firebase.database().ref('/wordlists/').once('value').then(function(snapshot) {
 		console.log("Accessed database");
-	var listGroupID = document.getElementById("wordlistgroup");
-	var listHeadingID = document.getElementById("listheading");
-	if (pagehash == null) {
-		listNo = parseInt(0);
-	} else {
-		listNo = parseInt(pagehash.substring(1)) - 1;
-	}
-	
-	snapshot.forEach(function(childSnapshot){
-		// DEBUG: console.log("key: " + childSnapshot.key + ", listNo: " + listNo);
-	if (childSnapshot.key == listNo){
-		currentChild = childSnapshot;
+		var listGroupID = document.getElementById("wordlistgroup");
+		var listHeadingID = document.getElementById("listheading");
+		
+		// For testing purposes:
+		if (pagehash == null) {
+			listNo = parseInt(0);
+		} else {
+			listNo = parseInt(pagehash.substring(1)) - 1;
+		}
+		
+		snapshot.forEach(function(childSnapshot){
+			// DEBUG: console.log("key: " + childSnapshot.key + ", listNo: " + listNo);
+			if (childSnapshot.key == listNo){
+				currentChild = childSnapshot;
 
-		listHeadingID.innerText = childSnapshot.val().name;
-		var list = childSnapshot.val().words;
-		for (var i = 0; i < list.length; i++) {
-			var li = document.createElement("li");
-			li.className = "list-group-item";
-			li.innerText = list[i];
-			listGroupID.appendChild(li);
-			//DEBUG: console.log("added item: " + list[i]);
-	}
-	}
+				// Create list item for each word
+				listHeadingID.innerText = childSnapshot.val().name;
+				var list = childSnapshot.val().words;
+				for (var i = 0; i < list.length; i++) {
+					var li = document.createElement("li");
+					li.className = "list-group-item";
+					li.innerText = list[i];
+					listGroupID.appendChild(li);
+					//DEBUG: console.log("added item: " + list[i]);
+				}
+			}
 
-})
+		})
 	}).catch(function(db_error) {
 		console.log("Error loading wordlists: " + db_error);
 
@@ -105,40 +127,55 @@ function populateWordListOnRecordAudio(){
 }
 
 
+/**
+* Uploaded given audio file, typically in .wav format, to the firebase storgae bucket
+*/
 function upload(AudioBLOB){
  	// Create a storage reference from our storage service
     var storageRef = storage.ref();
     var inputRef = storageRef.child('Input/');
     var uploadTask = storageRef.child('Input/' + tempFilepath).put(AudioBLOB);
-    console.log("File uploaded");
+    console.log("File uploaded: Input/" + tempFilepath + ".wav");
     alert("File submitted");
 
 }
+
+/**
+* Add a node to the session subtree to show that an audio file has been added
+* This will later be used by the Segmentor to watch for incoming file
+* Uses .set() from firebase databases
+* Function called by the  main audio recorder when submitting an audio file
+*/
 function createSession(date, filepath){
     // Create a session node in database
-    console.log(filepath);
-
-    var noSessions = null;
+    // Data respondes to data in the firebase database
     database.ref('sessions').once('value', function(snapshot) {
-    // noSessions = snapshot.numChildren(); 
-    //database.ref('sessions').once('value', function(snapshot) { console.log('Count: ' + snapshot.numChildren()); });
-  	console.log("No Sessions: " + noSessions);
-  	database.ref('sessions/' + (snapshot.numChildren() + 1)).set({
-    date : date,
-    filepath : "Input/".concat(filepath),
-    language : getHashValueLanguage(),
-    name : filepath,
-    scrapped : 0,
-    spliced : 0,
-    verified : 0,
-    wordlist : getHashValueKey()
-  });
-  console.log("Session node pushed to database");
+	    //database.ref('sessions').once('value', function(snapshot) { console.log('Count: ' + snapshot.numChildren()); });
+	   	// Gets the numder of nodes in the subtree and adds 1 to it to use as an identifyer
+	  	database.ref('sessions/' + (snapshot.numChildren() + 1)).set({
+	    date : date,
+	    filepath : "Input/".concat(filepath).concat(".wav"),
+	    language : getHashValueLanguage(),
+	    name : filepath,
+	    scrapped : 0,
+	    spliced : 0,
+	    verified : 0,
+	    wordlist : getHashValueKey()
+	  });
+	  /** DEBUG:
+	  * console.log("date:" + date);
+	  * console.log("filepath: " + filepath + ".wav");
+	  * console.log("language: " + getHashValueLanguage());
+	  * console.log("name: " + filepath);
+	  * console.log("wordlist: " + getHashValueKey);
+	  * console.log("Session node pushed to database");
+	  */
   });
 }
 
-
-function getDateFormat(date){        
+// Edit date format to suit the database pathname
+// Format YYYYmmDDhhMMss
+function getDateFormatForFile(date){        
     now = date;
     var yyyy = now.getFullYear();
     var mm = now.getMonth() < 9 ? "0" + (now.getMonth() + 1) : (now.getMonth() + 1); // getMonth() is zero-based
@@ -149,11 +186,28 @@ function getDateFormat(date){
     tempDate = now;
     return "".concat(yyyy).concat(mm).concat(dd).concat(hh).concat(min).concat(ss);
 }
+// Edit date format to suit 'date' label in database
+// Format: yyyy-MM-dd_HH:mm:ss
+function getDateFormatForLabel(date){
+	now = date;
+	var yyyy = now.getFullYear();
+    var mm = now.getMonth() < 9 ? "0" + (now.getMonth() + 1) : (now.getMonth() + 1); // getMonth() is zero-based
+    var dd  = now.getDate() < 10 ? "0" + now.getDate() : now.getDate();
+    var hh = now.getHours() < 10 ? "0" + now.getHours() : now.getHours();
+    var min = now.getMinutes() < 10 ? "0" + now.getMinutes() : now.getMinutes();
+    var ss = now.getSeconds() < 10 ? "0" + now.getSeconds() : now.getSeconds();
+    tempDate = now;
+    return "".concat(yyyy).concat("-"+mm).concat("-"+dd).concat("_"+hh).concat(":"+min).concat(":"+ss);
+}
+// Create the full file name
+// Format: "auct_list00_YYYYmmDDhhMMss.wav"
 function getNameFormat(date){
 	var listNo = getHashValueListNo();
-	tempFilepath = "auct_list".concat(getHashValueListNo()).concat("_").concat(date).concat(".wav");
+	tempFilepath = "auct_list".concat(getHashValueListNo()).concat("_").concat(date);
 	return tempFilepath;
 }
+
+// Get values needed for searching and uploading from url hash
 function getHashValueListNo(){
 	var ahash = window.location.hash.substring(1);
 	var values = ahash.split("#");
