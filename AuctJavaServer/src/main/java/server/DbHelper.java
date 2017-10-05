231a50d30496b8b1ceca4d588f6dd053102b3d65
Fergus <fergusdixon101@gmail.com>
2017-10-05 02:27:10 +0200

    Ironing of bugs

--- AuctJavaServer/src/main/java/server/DbHelper.java
+++ AuctJavaServer/src/main/java/server/DbHelper.java
@@ -3,6 +3,8 @@ package server;
 import com.google.firebase.database.*;
 import models.SegmentModel;
 import models.SessionModel;
+
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -18,74 +20,72 @@ public class DbHelper {
      * Fetch any sessions in the DB that have not yet been recorded
      * @return ArrayList of SessionModels
      */
-    protected synchronized ArrayList<SessionModel> newSessions(){
-        final ArrayList models= new ArrayList<SessionModel>();
+    protected synchronized ArrayList<SessionModel> newSessions(DataSnapshot snapshot){
+        final ArrayList<SessionModel> models= new ArrayList<>();
 
-        System.out.println("Getting DB reference");
+        System.out.println("Checking for unspliced sessions...");
         DatabaseReference ref = FirebaseDatabase
                 .getInstance()
                 .getReference();
-        System.out.println("Success, adding event listener...");
-        ref.child("sessions").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                Iterable<DataSnapshot> children = snapshot.getChildren();
-                models.clear();
+        //System.out.println("Success, adding event listener...");
 
-                //Loop through all sessions
-                for (DataSnapshot child : children){
-                    SessionModel model = child.getValue(SessionModel.class);
-                    if(model.getSpliced()==0) {
-                        //Add to returning array if not yet spliced
-                        model.setId(Integer.parseInt(child.getKey()));
-                        models.add(model);
-                    }
-                }
+        Iterable<DataSnapshot> children = snapshot.getChildren();
+        models.clear();
+
+        //Loop through all sessions
+        for (DataSnapshot child : children){
+            SessionModel model = child.getValue(SessionModel.class);
+            if(model.getSpliced()==0) {
+                //Add to returning array if not yet spliced
+                model.setId(Integer.parseInt(child.getKey()));
+                models.add(model);
             }
+        }
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-
-            }
-        });
-        System.out.println("Success");
-        System.out.println("Waiting for response, if this takes too long it may mean there are no new sessions to segment.");
+        //System.out.println("Success");
+        //System.out.println("Waiting for response, if this takes too long it may mean there are no new sessions to segment.");
 
         //Waiting for DB response
-        do{
-            System.out.println("...");
-            try {
-                wait(2000);
-            } catch (InterruptedException e) {
-                System.out.println("Interrupted");
-            }
-        } while (models.size()==0);
+//        do{
+//            System.out.println("...");
+//            try {
+//                wait(2000);
+//            } catch (InterruptedException e) {
+//                System.out.println("Interrupted");
+//            }
+//        } while (models.size()==0);
         System.out.println(models.size() + " unprocessed sessions found");
         return models;
-
     }
 
     /**
      * Mark the given session as successfully spliced
      * @param id of the session
      */
-    public void markSpliced(int id){
+    public void markSpliced(ArrayList<Integer> ids){
         DatabaseReference ref = FirebaseDatabase
                 .getInstance()
                 .getReference();
+
         Map<String, Object> splice = new HashMap<String, Object>();
         splice.put("spliced", 1);
+        Map<String, Object> sessions = new HashMap<String, Object>();
+        for(int id : ids){
+            sessions.put(Integer.toString(id), splice);
+        }
 
-        ref.child("sessions/"+id).updateChildren(splice);
+        ref.child("sessions").updateChildren(sessions);
     }
 
     /**
      * Make a record of the given session's corresponding segments in the DB
      * @param session to record
      */
-    public void recordSegments(SessionModel session){
+    public long recordSegments(SessionModel session, DataSnapshot wordlists, long index){
         Path directory = Paths.get("/home/fergus/AuCT/AuctJavaServer/src/output/"+session.getName());
         System.out.println("Recording segments in DB...");
+        final ArrayList<Long> startingIndex = new ArrayList<>();
+        startingIndex.add(index);
         try {
 
             DatabaseReference ref = FirebaseDatabase
@@ -103,10 +103,9 @@ public class DbHelper {
                 )-1;
 
                 // Getting all labels for the session
-                ArrayList<String> list = getLabels(listNum);
+                ArrayList<String> list = getLabels(listNum, wordlists.child(listNum+"/words"));
 
                 // Get the next available ID for segment
-                long startingIndex = getSegmentIndex();
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
                     String label;
@@ -125,13 +124,14 @@ public class DbHelper {
                     }
                     //Creating a new SegmentModel to update the DB with
                     SegmentModel segment = new SegmentModel(
-                            file.toString().substring(file.toString().indexOf("seg")),
+                            "Output/" + file.toString().substring(file.toString().indexOf("_au")) +"/" +
+                                    file.toString().substring(file.toString().indexOf("seg")),
                             label,
                             0,
                             session.getId(),
                             0);
-                    segs.put(Long.toString(startingIndex), segment);
-                    startingIndex++;
+                    segs.put(Long.toString(startingIndex.get(0)), segment);
+                    startingIndex.set(0, startingIndex.get(0)+1);
                     return FileVisitResult.CONTINUE;
                 }
 
@@ -145,6 +145,7 @@ public class DbHelper {
             e.printStackTrace();
         }
         System.out.println("Segment recorded in DB");
+        return startingIndex.get(0);
     }
 
 
@@ -152,33 +153,34 @@ public class DbHelper {
      * Get the next available segment ID
      * @return ID
      */
-    private synchronized long getSegmentIndex(){
+    public synchronized long getSegmentIndex(DataSnapshot snapshot){
         final ArrayList<Long> index = new ArrayList<>();
         index.add(0, Long.parseLong("-1"));
-        DatabaseReference ref = FirebaseDatabase
-                .getInstance()
-                .getReference();
-        ref.child("segments").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                index.set(0, snapshot.getChildrenCount());
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-
-            }
-        });
-
-        //Waiting for DB response
-        do{
-            System.out.println("...");
-            try {
-                wait(2000);
-            } catch (InterruptedException e) {
-                System.out.println("Interrupted");
-            }
-        } while (index.get(0)==-1);
+        index.set(0, snapshot.getChildrenCount());
+//        DatabaseReference ref = FirebaseDatabase
+//                .getInstance()
+//                .getReference();
+//        ref.child("segments").addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot snapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//
+//            }
+//        });
+//
+//        //Waiting for DB response
+//        do{
+//            System.out.println("...");
+//            try {
+//                wait(2000);
+//            } catch (InterruptedException e) {
+//                System.out.println("Interrupted");
+//            }
+//        } while (index.get(0)==-1);
         return index.get(0);
     }
 
@@ -187,27 +189,28 @@ public class DbHelper {
      * @param listNm The wordlist to load
      * @return ArrayList of labels
      */
-    private synchronized ArrayList<String> getLabels(int listNm){
+    private synchronized ArrayList<String> getLabels(int listNm, DataSnapshot snapshot){
         final ArrayList<String> words = new ArrayList<>();
-        DatabaseReference ref = FirebaseDatabase
-                .getInstance()
-                .getReference();
-        ref.child("wordlists/"+listNm + "/words").addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot snapshot) {
-                ArrayList<String> list = (ArrayList)snapshot.getValue(); //Getting the words
-                words.addAll(list); //Add them to our final variable
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-
-            }
-        });
+        ArrayList<String> list = (ArrayList)snapshot.getValue(); //Getting the words
+        words.addAll(list); //Add them to our final variable
+//        DatabaseReference ref = FirebaseDatabase
+//                .getInstance()
+//                .getReference();
+//        ref.child("wordlists/" +listNm + "/words").addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot snapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//
+//            }
+//        });
 
         //Waiting for DB response
         do{
-            System.out.println("...");
+            System.out.println("Waiting for labels...");
             try {
                 wait(2000);
             } catch (InterruptedException e) {
